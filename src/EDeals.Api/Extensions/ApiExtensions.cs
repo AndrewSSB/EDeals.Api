82d5ccc519476f1c77c2555f1b344cd08de49542
Andrei Ene <95134095+AndrewSSB@users.noreply.github.com>
2023-06-08 21:46:40 +0300

    sdfgbadsbgsadkjfs

--- src/EDeals.Api/Extensions/ApiExtensions.cs
+++ src/EDeals.Api/Extensions/ApiExtensions.cs
@@ -6,7 +6,7 @@
         {
             builder.Configuration.AddJsonFile("appsettings.json", false, true);
             builder.Configuration.AddJsonFile("appsettings.Local.json", true, true);
-            builder.Configuration.AddJsonFile($"appsettings.{builder.Environment}.json", false, true);
+            builder.Configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", false, true);
             builder.Configuration.AddEnvironmentVariables();
         }
     }
