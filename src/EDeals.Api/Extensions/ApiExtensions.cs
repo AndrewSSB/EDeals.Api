342c08921527a24acf28fc518299db71973eb75c
Andrei Ene <95134095+AndrewSSB@users.noreply.github.com>
2023-06-08 21:41:39 +0300

    sfhbuasfasd

--- src/EDeals.Api/Extensions/ApiExtensions.cs
+++ src/EDeals.Api/Extensions/ApiExtensions.cs
@@ -5,7 +5,7 @@
         public static void AddApplicationSettings(WebApplicationBuilder builder)
         {
             builder.Configuration.AddJsonFile("appsettings.json", false, true);
-            builder.Configuration.AddJsonFile("appsettings.Local.json", false, true);
+            builder.Configuration.AddJsonFile("appsettings.Local.json", true, true);
             builder.Configuration.AddJsonFile($"appsettings.{builder.Environment}.json", false, true);
             builder.Configuration.AddEnvironmentVariables();
         }
